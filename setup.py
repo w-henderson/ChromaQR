a597e68baf2b84f558b123c97e1dcd8b437c62b1
William Henderson <william-henderson@outlook.com>
2021-02-18 15:36:53 +0000

    Set up boilerplate code

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="ChromaQR",
+    version="0.0.1",
+    description="Get three times the data into a QR code with RGB.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="w-henderson",
+    packages=["chromaqr"],
+    package_dir={"": "."},
+    entry_points={"console_scripts": ["chromaqr = chromaqr.cli:main"]},
+    zip_safe=False,
+    install_requires=[
+        "pyzbar",
+        "colorama",
+        "qrcode",
+        "pillow"
+    ],
+    extras_require={
+        "tests": ["pytest"],
+    },
+    python_requires=">=3.6",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    include_package_data=True
+)
